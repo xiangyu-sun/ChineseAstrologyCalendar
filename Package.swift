5d164f4c6a81b279736e8eccea8b2657056c7c80
Xiangyu Sun <luc.alexander.sun@icloud.com>
2021-10-16 23:55:13 +0800

    add platform versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ChineseAstrologyCalendar",
+    platforms: [.iOS(.v13), .watchOS(.v4), .macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
