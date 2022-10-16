6d61bd57a7cf20c5cc4668b1eaed7487246d32ca
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-10-16 17:04:19 +0200

    add to chinese char for wuxing

--- Sources/ChineseAstrologyCalendar/wuxing.swift
+++ Sources/ChineseAstrologyCalendar/wuxing.swift
@@ -13,4 +13,19 @@ public enum Wuxing {
     case shui
     case huo
     case tu
+    
+    public var chineseCharacter: String {
+        switch self {
+        case .jin:
+            return "金"
+        case .mu:
+            return "木"
+        case .shui:
+            return "水"
+        case .huo:
+            return "火"
+        case .tu:
+            return "土"
+        }
+    }
 }
