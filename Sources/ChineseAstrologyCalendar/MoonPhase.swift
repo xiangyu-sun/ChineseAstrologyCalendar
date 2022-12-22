144d88290269fe45316512a80aac0ba1e831e131
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-12-22 22:10:03 +0100

    adding conversion to weather kit moon phase

--- Sources/ChineseAstrologyCalendar/MoonPhase.swift
+++ Sources/ChineseAstrologyCalendar/MoonPhase.swift
@@ -2,51 +2,27 @@
 import WeatherKit
 
 @available(iOS 16.0, macOS 13.0, watchOS 9.0, *)
-extension MoonPhase {
-  public func acientChineseName(_ day: Day) -> String {
+public extension ChineseMoonPhase {
+  var moonPhase: MoonPhase {
     switch self {
-    case .new:
-      if day == .sanshi {
-        return "晦"
-      } else {
-        return "朔"
-      }
-
-    case .waxingCrescent:
-      return "蛾眉月"
-    case .firstQuarter:
-      return "上弦月"
-    case .waxingGibbous:
-      return "漸盈凸月"
-    case .full:
-      return "望"
-    case .waningGibbous:
-      return "漸虧凸月"
-    case .lastQuarter:
-      return "下弦月"
-    case .waningCrescent:
-      return "殘月"
-    }
-  }
-
-  public func modernChineseName(_: Day) -> String {
-    switch self {
-    case .new:
-      return "新月"
-    case .waxingCrescent:
-      return "上蛾眉月"
-    case .firstQuarter:
-      return "上弦月"
-    case .waxingGibbous:
-      return "漸盈凸月"
-    case .full:
-      return "滿月"
-    case .waningGibbous:
-      return "漸虧凸月"
-    case .lastQuarter:
-      return "下弦月"
-    case .waningCrescent:
-      return "下蛾眉月"
+    case .朔:
+      return .new
+    case .蛾眉月:
+      return .waxingCrescent
+    case .上弦月:
+      return .firstQuarter
+    case .漸盈凸月:
+      return .waxingGibbous
+    case .望:
+      return .full
+    case .漸虧凸月:
+      return .waningGibbous
+    case .下弦月:
+      return .lastQuarter
+    case .殘月:
+      return .lastQuarter
+    case .晦:
+      return .new
     }
   }
 
