7d4151af1b0cdc3454ae6fbcfb1555aac8445674
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-12-25 16:14:55 +0100

    adding next startDate

--- Sources/ChineseAstrologyCalendar/Shichen.swift
+++ Sources/ChineseAstrologyCalendar/Shichen.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public struct Shichen {
+
+  // MARK: Public
+
   public let dizhi: Dizhi
   public let date: Date
   public let ke: Double = 15 * 60
@@ -49,5 +52,24 @@ public struct Shichen {
     endDP.second = 59
 
     return Calendar.current.date(from: endDP) ?? Date()
+  }
+
+    public var nextStartDate: Date {
+    let startHour = dizhi.next.hourInterval.start
+
+    var startDP = Calendar.current.dateComponents(
+      [.year, .month, .day,.hour, .minute, .second, .nanosecond],
+      from: date)
+
+    if startHour == 23, startDP.hour == 0 {
+      startDP.day = startDP.day! - 1
+    }
+
+    startDP.hour = startHour
+    startDP.minute = 0
+    startDP.second = 0
+    startDP.nanosecond = 0
+
+    return Calendar.current.date(from: startDP) ?? Date()
   }
 }
