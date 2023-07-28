8800b6d17e86fc719b6700b6c4321f30cb4e65ab
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-07-28 20:55:07 +0200

    update atch oS min version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ChineseAstrologyCalendar",
-    platforms: [.iOS(.v13), .watchOS(.v4), .macOS(.v10_14)],
+    platforms: [.iOS(.v13), .watchOS(.v5), .macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
