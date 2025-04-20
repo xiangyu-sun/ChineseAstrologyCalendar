fb8d33532349aa033b7d81496b4c5882ca659e8e
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-04-20 19:17:47 +0200

    update watch OS min version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ChineseAstrologyCalendar",
-    platforms: [.iOS(.v13), .watchOS(.v5), .macOS(.v10_14)],
+    platforms: [
+      .iOS(.v13),
+      .watchOS(.v5),
+      .macOS(.v10_14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
