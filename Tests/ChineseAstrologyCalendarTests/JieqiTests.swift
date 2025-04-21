ac509e3d9ca3617a195a08e0b113195d8a223277
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-04-21 17:24:06 +0200

    Update JieqiTests.swift
    fixing tests

--- Tests/ChineseAstrologyCalendarTests/JieqiTests.swift
+++ Tests/ChineseAstrologyCalendarTests/JieqiTests.swift
@@ -21,7 +21,7 @@ final class JieqiTests: XCTestCase {
   func testExample() throws {
     XCTAssertEqual(
       Jieqi.allCases.map(\.celestialLongitude),
-      [315, 330, 345, 0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300])
+      [300, 315, 330, 345, 0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285])
   }
   
   func testMonthFromCelestialLongitudeMapping() {
