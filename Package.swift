de7f24b25f69555c0fe269fac2da5170870c464a
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-07-15 17:49:35 +0200

    update PSM specification

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
       .package(url: "https://github.com/airbnb/swift", from: "1.0.0"),
       .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
-      .package(url: "git@github.com:xiangyu-sun/Astral.git", .branch("main")),
+      .package(url: "git@github.com:xiangyu-sun/Astral.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
