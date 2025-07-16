b4d31db992ce5a1410c68dd1ac037db1ee5ffc2a
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-07-16 17:20:49 +0200

    adding Four Simles and 28 Lunar Mansion

---
+++ Sources/ChineseAstrologyCalendar/LunarMansion.swift
@@ -0,0 +1,74 @@
+
+import Foundation
+import Astral
+
+/// Represents the Four Symbols (四象) corresponding to groups of lunar mansions.
+public enum FourSymbol: String {
+    case azureDragon   = "青龙"
+    case vermilionBird = "朱雀"
+    case whiteTiger    = "白虎"
+    case blackTortoise = "玄武"
+}
+
+// MARK: - Lunar Mansion (二十八宿)
+
+/// Represents one of the 28 lunar mansions (二十八宿).
+public enum LunarMansion: String, CaseIterable {
+  case jiaoXiu   = "角宿"
+  case kangXiu   = "亢宿"
+  case diXiu     = "氐宿"
+  case fangXiu   = "房宿"
+  case xinXiu    = "心宿"
+  case wei4Xiu   = "胃宿"   // 胃宿, wèi xiù (4th tone)
+  case tailXiu   = "尾宿"
+  case jiXiu     = "箕宿"
+  case jingXiu   = "井宿"
+  case guiXiu    = "鬼宿"
+  case liuXiu    = "柳宿"
+  case xingXiu   = "星宿"
+  case zhangXiu  = "张宿"
+  case yiXiu     = "翼宿"
+  case zhenXiu   = "轸宿"
+  case kuiXiu    = "奎宿"
+  case louXiu    = "娄宿"
+  case maoXiu    = "昴宿"
+  case biXiu     = "毕宿"
+  case ziXiu     = "觜宿"
+  case shenXiu   = "参宿"
+  case douXiu    = "斗宿"
+  case niuXiu    = "牛宿"
+  case nuXiu     = "女宿"
+  case xuXiu     = "虚宿"
+  case wei1Xiu   = "危宿"   // 危宿, wēi xiù (1st tone)
+  case shiXiu    = "室宿"
+  case wallXiu   = "壁宿"
+  
+  /// Maps the Moon's true ecliptic longitude to one of the 28 lunar mansions (二十八宿).
+  /// - Parameter jd2000: Julian Day offset from J2000.0 (in days).
+  /// - Returns: The name of the lunar mansion.
+  func lunarMansion(date: Date) -> LunarMansion {
+    
+    let rev = moon_true_longitude(jd2000: date.toJC2000)
+    // Convert revolution fraction to an index 0...27
+    let idx = Int(floor(rev * 28.0)) % 28
+    return LunarMansion.allCases[idx]
+  }
+
+
+}
+
+extension LunarMansion {
+  /// Returns the FourSymbol (四象) associated with this lunar mansion.
+  public var fourSymbol: FourSymbol {
+    switch self {
+    case .jiaoXiu, .kangXiu, .diXiu, .fangXiu, .xinXiu, .tailXiu, .jiXiu:
+      return .azureDragon
+    case .jingXiu, .guiXiu, .liuXiu, .xingXiu, .zhangXiu, .yiXiu, .zhenXiu:
+      return .vermilionBird
+    case .kuiXiu, .louXiu, .wei4Xiu, .maoXiu, .biXiu, .ziXiu, .shenXiu:
+      return .whiteTiger
+    case .douXiu, .niuXiu, .nuXiu, .xuXiu, .wei1Xiu, .shiXiu, .wallXiu:
+      return .blackTortoise
+    }
+  }
+}
